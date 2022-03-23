06ea8ea8ee33dbeb070ce099ac76d11ac202b355
Tasha <Tasha@TASHA-LAPTOP>
2022-03-23 11:07:48 -0400

    Add project files.

---
+++ Guy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace JoeAndBob
+{
+    internal class Guy
+    {
+        public string Name;
+        public int Cash;
+        /// <summary>
+        /// Writes my name and the amount of cash I have to the console. 
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            WriteLine(Name + " has " + Cash + " dollars.");
+        }
+        /// <summary>
+        /// Gives some of my cash, removing it from my wallet (or printing a msg to the console if i don't have enough). 
+        /// </summary>
+        /// <param name="amount">Amount of Cash to give.</param>
+        /// <returns>
+        /// The amount of cash removed from my wallet or 0 if i don't have enough cash (or if amount is invalid).
+        /// </returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                WriteLine(Name + " says: " + amount + " isn't a valid amount. ");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                WriteLine(Name + " says: " + " I don't have enough to give " + amount);
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+        /// <summary>
+        /// Receive some cash, adding it to my wallet (or printing a message to the console if the amount is invalid).
+        /// </summary>
+        /// <param name="amount">Amount of Cash to give.</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                WriteLine(Name + " says: " + amount + " isn't an amount i'll take. ");
+            }
+            else { Cash += amount; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace JoeAndBob
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Create a new Guy obj in a variable called joe
+             * Set its Name field to "Joe"
+             * Set its Cash field to 50
+             */
+            Guy joe = new Guy() { Cash = 50, Name = "Joe" };
+            /* Create a new Guy obj in a variable called joe
+             * Set its Name field to "Bob"
+             * Set its Cash field to 100
+             */
+            Guy bob = new Guy() { Cash = 100, Name = "Bob" };
+
+            while (true)
+            {
+                // Call the WriteMyInfo methods for each Guy obj
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                WriteLine("Enter an amount: ");
+                string howMuch = ReadLine();
+                if (howMuch == "") return;
+                /* Use int.TryParse to try to convert the howMuch string to an int 
+                 * If successful...*/
+                if(int.TryParse(howMuch, out int amount))
+                {
+                    WriteLine("Who should give the cash: ");
+                    string whichGuy = ReadLine();   
+                    if (whichGuy == "Joe")
+                    {
+                        // Call the joe obj GiveCash method and save the results 
+                        int cashGiven = joe.GiveCash(amount);
+
+                        // Call the bob obj ReceiveCash method with the saved results 
+                        bob.ReceiveCash(cashGiven);
+
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        // Call the bob obj GiveCash method and save the results 
+                        int cashGiven = bob.GiveCash(amount);
+                        // Call the joe obj ReceiveCash method with the saved results */
+                        joe.ReceiveCash(cashGiven);
+                        
+                    }
+                    else { WriteLine("Please enter 'Joe' or 'Bob' "); }
+                }
+                else { WriteLine("Please enter an amount (or a blank line to exit). "); }
+            }
+        }
+    }
+}
